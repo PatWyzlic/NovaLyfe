1cd1b2249f6fd73d938bd30b8c691d704d831f66
patwyzlic <patrickwyzlic@gmail.com>
2022-07-24 19:10:18 -0400

    Creation of front and backend

---
+++ Backend/env/novalyfe/novalyfe/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import include, re_path # For Django 4.0+
+from core.views import *
+  
+urlpatterns = [
+    re_path('admin/', admin.site.urls),
+    re_path('wel/', ReactView.as_view(), name="something"),
+]
