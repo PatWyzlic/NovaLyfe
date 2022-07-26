9330f1bb5aaf9f450102181f79f432311485489d
patwyzlic <patrickwyzlic@gmail.com>
2022-07-26 10:56:55 -0400

    Navbar updated to relevant links

--- Frontend/novalyfe/src/components/NavBar.js
+++ Frontend/novalyfe/src/components/NavBar.js
@@ -3,12 +3,12 @@ import AuthContext from "../context/AuthContext";
 
 export default function NavBar(){
     return(
-        <div>
-            <nav>
-                <Link to="/protected"><h1>ProtectedPage</h1></Link>
-                <a><h1>NovaLyfe</h1></a>
-            </nav>
-        </div>
+        <nav className="navbar navbar-expand-lg bg-light ">
+            <a className="navbar-brand" href="/"><h1>NovaLyfe</h1></a>
+            <Link to="/protected"><h1 className="navbar-nav me-auto mb-2 mb-lg-0 ">Protected</h1></Link>
+            <Link to="/login"><h1 className="navbar-nav me-auto mb-2 mb-lg-0">Login</h1></Link>
+            <Link to="/register"><h1 className="navbar-nav me-auto mb-2 mb-lg-0">Register</h1></Link>
+        </nav>
     )
 }
 
