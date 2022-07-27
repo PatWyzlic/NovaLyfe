b86622d9236727c500510142cdab4e4dd6ca4925
patwyzlic <patrickwyzlic@gmail.com>
2022-07-27 14:01:14 -0400

    Formatting for Navbar

--- Frontend/novalyfe/src/components/NavBar.jsx
+++ Frontend/novalyfe/src/components/NavBar.jsx
@@ -2,27 +2,36 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 import "../components/NavBar"
+import Nav from 'react-bootstrap/Nav';
 
 export default function NavBar(){
     const { user, logoutUser } = useContext(AuthContext);
     return(
-        <nav className="navbar navbar-expand-lg bg-light ">
-            <div>
-                <div>
+        <nav className="navbar navbar-expand-lg text-dark justify-content-center">
+            <div id="navbarNav">
                 {user ? (
-                <>
-                    <a className="navbar-brand" href="/"><h1>NovaLyfe</h1></a>
-                    <Link to="/protected"><h1>Protected</h1></Link>
-                    <button onClick={logoutUser}>Logout</button>
-                </> 
+                    <ul className="navbar-nav">
+                        <li className="nav-link">
+                            <button onClick={logoutUser} className="navbar-item active">Logout</button>
+                        </li>
+                    </ul>       
                 ) : (
-                    <>
-                        <a className="navbar-brand" href="/"><h1>NovaLyfe</h1></a>
-                        <Link to="/login"><h1 className="navbar-nav me-auto mb-2 mb-lg-0">Login</h1></Link>
-                        <Link to="/register"><h1 className="navbar-nav me-auto mb-2 mb-lg-0">Register</h1></Link>
-                    </>
+                    <ul className="navbar-nav" >
+                        <li className="nav-link">
+                            <Link to="/login"> 
+                                <h1     className="text-secondary">
+                                    Login
+                                </h1>
+                            </Link>
+                        </li>
+                        <li className="nav-link">
+                            <Link to="/register"> 
+                                <h1 className="text-secondary">Register
+                                </h1>
+                            </Link>
+                        </li>
+                    </ul>
                 )}
-                </div>
             </div>
         </nav>
     )
