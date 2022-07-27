4e62354a95ff6ed97f8aaa55ec52035a6aa4b5ec
patwyzlic <patrickwyzlic@gmail.com>
2022-07-27 17:56:34 -0400

    Update to models

--- Backend/env/novalyfe/core/admin.py
+++ Backend/env/novalyfe/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from .models import Profile, ToDo, RoutineItem, Animal
+from .models import ToDo, RoutineItem, Animal, Profile
 
 admin.site.register(Profile)
 admin.site.register(ToDo)
--- Backend/env/novalyfe/core/views.py
+++ Backend/env/novalyfe/core/views.py
@@ -1,5 +1,6 @@
 # api/views.py
 
+from urllib import request
 from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
@@ -10,18 +11,17 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny, IsAuthenticated
-
+from . serializer import *
+from rest_framework.views import APIView
 # Create your views here.
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
-
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny]
     serializer_class = RegisterSerializer
-
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -29,10 +29,17 @@ def getRoutes(request):
         '/api/token/',
         '/api/register/',
         '/api/token/refresh/',
-        '/api/prediction/'
+        '/api/prediction/',
     ]
     return Response(routes)
 
+def AnimalView(request):
+    serializer_class = AnimalSerializer
+    
+    def get(self, request):
+        data = [ {"name": data.name, "description": data.description, "color": data.color, "rec_format": data.rec_format, "breed": data.breed} 
+        for data in Animal.objects.all()]
+        return Response(data)
 
 @api_view(['GET', 'POST'])
 @permission_classes((IsAuthenticated,))
