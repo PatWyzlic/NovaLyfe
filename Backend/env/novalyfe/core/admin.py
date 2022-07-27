a39bf197a34a2a56cafa7ca34b033843180a2ba4
patwyzlic <patrickwyzlic@gmail.com>
2022-07-27 14:00:07 -0400

    Addition of models to Admin Page

--- Backend/env/novalyfe/core/admin.py
+++ Backend/env/novalyfe/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Profile, ToDo, RoutineItem, Animal
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(ToDo)
+admin.site.register(RoutineItem)
+admin.site.register(Animal)
