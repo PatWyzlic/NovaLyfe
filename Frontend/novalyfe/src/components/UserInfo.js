9982e73936f7bf226201d19f46381b72614645d0
patwyzlic <patrickwyzlic@gmail.com>
2022-07-29 20:41:59 -0400

    Can receive profile information of all users

--- Frontend/novalyfe/src/components/UserInfo.js
+++ Frontend/novalyfe/src/components/UserInfo.js
@@ -1,11 +1,26 @@
-
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function UserInfo({ user }) {
+
+  const [profile, setProfile] = useState([])
+  
+  function getData(){
+      const url = 'http://127.0.0.1:8000/api/profile/'
+      axios.get(url)
+      .then(response => 
+        setProfile(response.data));
+  }
+  
+  useEffect(() => {
+      getData();
+  }, []);
+
+
     return (
       <div>
         <h1>Hello: {user.username}</h1>
-        {console.log(user)}
+        {console.log(profile)}
       </div>
     );
   }
