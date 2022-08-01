f1cda5cacc12b671d7ab196796ce1793f0f82790
patwyzlic <patrickwyzlic@gmail.com>
2022-08-01 15:53:05 -0400

    Additional paths added to main urls App.jsx

--- Frontend/novalyfe/src/App.jsx
+++ Frontend/novalyfe/src/App.jsx
@@ -9,16 +9,26 @@ import UserInfo from "./components/UserInfo";
 import AuthContext, { AuthProvider } from "./context/AuthContext";
 import ProtectedPage from "./utils/utils_ProtectedPage";
 import RegisterPage from "./views/views_RegisterPage";
+import ToDoCreate from "./views/views_ToDoCreatePage";
+import ToDoPage from "./views/views_ToDoPage";
+import ToDoSinglePage from "./views/ToDoSinglePage";
+import WeatherPage from "./views/views_WeatherPage"
+import RoutinePage from "./views/views_RoutinePage"
+const API_KEY =`${process.env.REACT_APP_API_KEY}`
 
 function App() {
     return (
       <AuthProvider>
         <NavBar />
         <Routes>
-          <Route path="/protected" element={<ProtectedPage/>}/>
+          <Route path="/todos/" element={<ToDoPage/>}/>
+          <Route path="/todos/:id/" element={<ToDoSinglePage/>} />
+          <Route path="/todos/create/" element={<ToDoCreate/>}/>
+          <Route path="/routines/" element={<RoutinePage/>}/>
+          <Route path="/weatherpage/" element={<WeatherPage API_KEY={API_KEY}/>}/>
           <Route path="/" element={<HomePage/>}/>
-          <Route path="/register" element={<RegisterPage/>}/>
-          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/register/" element={<RegisterPage/>}/>
+          <Route path="/login/" element={<LoginPage/>}/>
         </Routes>
       </AuthProvider>
     );
