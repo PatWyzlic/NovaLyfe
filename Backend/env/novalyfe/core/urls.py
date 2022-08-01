850b383515d3cd21830d78d971e2e6e5d79b72d1
patwyzlic <patrickwyzlic@gmail.com>
2022-08-01 15:11:48 -0400

    todos paths added for singleview, create

--- Backend/env/novalyfe/core/urls.py
+++ Backend/env/novalyfe/core/urls.py
@@ -10,13 +10,23 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', views.RegisterView.as_view(), name='auth_register'),
-    path('register/', views.EndPoint, name='register'),
     path('', views.getRoutes),
+
+    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('register/', views.RegisterView.as_view(), name='auth_register'),
+
+    path('register/', views.EndPoint, name='register'),
+
     path('todos/', views.ToDoView.as_view(), name="todos"),
-    path('todos/create', views.CreateToDo, name="create-todo"),
-    path('edittodo/<str:pk>/', views.ToDoView.as_view(), name="todos"),
+
+    path('todos/<str:id>/', views.getToDo.as_view(), name="todo_view"),
+
+    path('todos/create/', views.createToDo.as_view(), name="todocreate_view"),
+
     path('routines/', views.RoutineView.as_view(), name="routines"),
+
+    path('weatherpage/', views.WeatherPage, name="weatherpage"),
 ]
