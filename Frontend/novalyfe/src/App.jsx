ef05b77085801852e587ea262d3cb1ab9894f718
patwyzlic <patrickwyzlic@gmail.com>
2022-08-02 01:27:44 -0400

    Routes updated in frontend

--- Frontend/novalyfe/src/App.jsx
+++ Frontend/novalyfe/src/App.jsx
@@ -1,20 +1,20 @@
 import React, {Component} from "react";
 import { useState, useContext } from "react";
-import axios from "axios";
+
 import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from './views/views_HomePage'
 import NavBar from "./components/NavBar"
 import LoginPage from "./views/views_LoginPage";
 import UserInfo from "./components/UserInfo";
 import AuthContext, { AuthProvider } from "./context/AuthContext";
-import ProtectedPage from "./utils/utils_ProtectedPage";
 import RegisterPage from "./views/views_RegisterPage";
 import ToDoCreate from "./views/views_ToDoCreatePage";
 import ToDoPage from "./views/views_ToDoPage";
 import ToDoSinglePage from "./views/ToDoSinglePage";
 import WeatherPage from "./views/views_WeatherPage"
 import RoutinePage from "./views/views_RoutinePage"
-
+import ToDoEditPage from "./views/views_ToDoEditPage"
+import DeleteView from "./views/DeleteView"
 
 function App() {
     return (
@@ -23,6 +23,8 @@ function App() {
         <Routes>
           <Route path="/todos/" element={<ToDoPage/>}/>
           <Route path="/todos/:id/" element={<ToDoSinglePage/>} />
+          <Route path="/todos/delete/:id/" element={<DeleteView/>} />
+          <Route path="/todos/edit/:id/" element={<ToDoEditPage/>}/>
           <Route path="/todos/create/" element={<ToDoCreate/>}/>
           <Route path="/routines/" element={<RoutinePage/>}/>
           <Route path="/weatherpage/" element={<WeatherPage/>}/>
